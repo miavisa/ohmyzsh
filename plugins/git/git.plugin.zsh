2bc2b2545238799f6a1584cfbdc35d0bed4e01fe
Miguel Villanueva <mvillanueva@alertlogic.com>
2015-03-13 10:16:57 -0500

    added my theme

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -114,6 +114,33 @@ alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
 alias gcpa='git cherry-pick --abort'
 alias gcpc='git cherry-pick --continue'
+alias gcl='git config --list'
+alias gchp='git cherry-pick'
+compdef _git gchp=git-cherry-pick
+alias glg='git log --stat --max-count=10'
+compdef _git glg=git-log
+alias glgg='git log --graph --max-count=10'
+compdef _git glgg=git-log
+alias glgga='git log --graph --decorate --all'
+compdef _git glgga=git-log
+alias glo='git log --oneline --decorate --color'
+compdef _git glo=git-log
+alias glog='git log --oneline --decorate --color --graph'
+compdef _git glog=git-log
+alias gss='git status -s'
+compdef _git gss=git-status
+alias ga='git add'
+compdef _git ga=git-add
+alias gap='git add --patch'
+alias gaa='git add --all'
+alias gm='git merge'
+compdef _git gm=git-merge
+alias grh='git reset HEAD'
+alias grhh='git reset HEAD --hard'
+alias gclean='git reset --hard && git clean -dfx'
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+
+# Sign and verify commits with GPG
 alias gcs='git commit -S'
 alias gcss='git commit -S -s'
 alias gcssm='git commit -S -s -m'
